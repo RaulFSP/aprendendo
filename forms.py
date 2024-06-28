e118c2e15a34281597dd1471c0aeeb68ebb293ee
Raul <r4ul.pereira@yandex.com>
2024-06-27 22:57:05 -0400

    um

--- forms.py
+++ forms.py
@@ -3,21 +3,21 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField, Valid
 from wtforms.validators import DataRequired, length, EqualTo
 
 class UserForm(FlaskForm):
-    name = StringField(label="Enter your name", validators=[DataRequired(),length(min=3,max=100)])
-    username = StringField(label="Enter your username", validators=[DataRequired(),length(min=3,max=30)])
-    email = StringField(label="Enter your email", validators=[DataRequired(),length(min=3,max=60)])
-    password = PasswordField(label="Enter a password", validators=[DataRequired(), length(min=5,max=30), EqualTo("password_test")])
-    password_test = PasswordField(label="Enter the same password",validators=[DataRequired(),length(min=5,max=30)])
+    name = StringField(label="Digite seu nome", validators=[DataRequired(),length(min=3,max=100)])
+    username = StringField(label="Digite um username", validators=[DataRequired(),length(min=3,max=30)])
+    email = StringField(label="Digite seu email", validators=[DataRequired(),length(min=3,max=60)])
+    password = PasswordField(label="Digite uma senha", validators=[DataRequired(), length(min=5,max=30), EqualTo("password_test")])
+    password_test = PasswordField(label="Confirme a senha",validators=[DataRequired(),length(min=5,max=30)])
     submit = SubmitField()
 
 class LoginForm(FlaskForm):
-    username = StringField(label="Enter your username", validators=[DataRequired(),length(min=3,max=30)])
-    password = PasswordField(label="Enter your password", validators=[DataRequired(), length(min=5,max=30)])
+    username = StringField(label="Digite seu username", validators=[DataRequired(),length(min=3,max=30)])
+    password = PasswordField(label="Senha", validators=[DataRequired(), length(min=5,max=30)])
     submit = SubmitField()
 
 class UserPostForm(FlaskForm):
-    title = StringField(label="Title",validators=[DataRequired(),length(min=2,max=30)])
-    author = StringField(label="Author",validators=[DataRequired(),length(min=2,max=30)])
-    slug =  StringField(label="Slug",validators=[DataRequired(),length(min=2,max=30)])
-    content = TextAreaField(label="Content", validators=[DataRequired(), length(min=1)])
+    title = StringField(label="Título da Postagem",validators=[DataRequired(),length(min=2,max=30)])
+    author = StringField(label="Autor",validators=[DataRequired(),length(min=2,max=30)])
+    slug =  StringField(label="Assunto",validators=[DataRequired(),length(min=2,max=30)])
+    content = TextAreaField(label="Conteúdo", validators=[DataRequired(), length(min=1)])
     submit = SubmitField()
--- routes.py
+++ routes.py
@@ -3,12 +3,15 @@ from flask import render_template, redirect, url_for, flash
 from models import UserModel, UserPostModel
 from forms import UserForm, LoginForm, UserPostForm
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_required, logout_user, current_user, login_user
 
-
-
-login_manager.login_view = "login_user"
+# flask login
+login_manager.login_view = "login"
 @login_manager.user_loader
+def load_user(user_id):
+    return UserModel.query.get_or_404(user_id)
 
+# password hash
 @property
 def password(self):
     raise AttributeError("Password not readable")
@@ -19,6 +22,8 @@ def password(self, password):
 
 def verify_password(self,password):
     return check_password_hash(self.password_hash, password)
+
+
 
 @app.route('/')
 def index():
@@ -45,13 +50,16 @@ def add_user():
         users = UserModel.query.order_by(UserModel.id).all()
         return render_template('user.html',form=form,users=users)
 
+#rota quebrada, precisa de atualizar o form de senha
 @app.route('/alter_user/<int:id>', methods=['POST','GET'])
+@login_required
 def alter_user(id):
     form = UserForm()
     user = UserModel.query.get_or_404(id)
     if form.validate_on_submit():
         user.name = form.name.data
         user.email = form.email.data
+        user.username = form.username.data
         form = UserForm(formdata=None)
         db.session.commit()
         flash(f'Usuário {user.name} alterado!')
@@ -59,20 +67,10 @@ def alter_user(id):
     else:
         return render_template('alter_user.html',form=form, user=user)
 
-@app.route('/login',methods=['POST','GET'])
-def login_user():
-    form = LoginForm()
-    user = None
-    check = None
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        form = LoginForm(formdata=None)
-        user = UserModel.query.filter_by(username=username).first_or_404()
-        check = check_password_hash(user.password_hash,password)
-    return render_template('login.html',form=form, user=user, check = check)
+
 
 @app.route('/add_post',methods=['POST','GET'])
+@login_required
 def add_post():
     form = UserPostForm()
     if form.validate_on_submit():
@@ -92,6 +90,7 @@ def add_post():
         return render_template('user_post.html',form=form)
 
 @app.route('/post/edit/<int:id>',methods=['POST','GET'])
+@login_required
 def alter_post(id):
     form = UserPostForm()
     post = UserPostModel.query.get_or_404(id)
@@ -112,9 +111,49 @@ def alter_post(id):
         return render_template('alter_post.html',form=form, post=post)
 
 @app.route('/post/delete/<int:id>')
+@login_required
 def delete_post(id):
     post = UserPostModel.query.get_or_404(id)
     db.session.delete(post)
     db.session.commit()
     flash(f"Post deletado!")
     return redirect(url_for('index'))
+
+
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        user = UserModel.query.filter_by(username=username).first()
+        if user:
+            password = form.password.data
+            if check_password_hash(user.password_hash, password):
+                login_user(user)
+                flash(f"{user.username} logado!")
+                form = LoginForm(formdata=None)
+                return redirect(url_for('index'))
+            else:
+                flash("Senha incorreta!")
+                return redirect(url_for('login'))
+        else:
+            flash("Usuário não encontrado!")
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html',form=form)
+
+@app.route('/logout',methods=['POST','GET'])
+@login_required
+def logout():
+    flash(f"Usuário {current_user.username} deslogado")
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/dashboard', methods=['POST','GET'])
+@login_required
+def dashboard_user():
+    username=current_user.username
+    user = UserModel.query.filter_by(username=username).first_or_404()
+    return render_template('dashboard.html',user=user)
