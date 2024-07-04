48e8be7ac43ec170604a45b667a826b33218acab
raul <raulfs@protonmail.com>
2024-07-04 16:10:38 -0400

    denovo no laptop

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
+# SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
+SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".format(
+    "mysql+pymysql",
+    "root",
+    "12345678",
+    "localhost",
+    "3306",
+    "integrador"
+)
 SECRET_KEY = "1235ceSD"
 UPLOAD_FOLDER = 'static/temp/'
